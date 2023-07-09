9b28c6da12b59f9100f68640fba784fa59e4a1ce
harsh bansal <hbansal@datopic.com>
2023-07-10 00:34:38 +0530

    message notification

--- crm/lead/urls.py
+++ crm/lead/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('add-lead/', views.add_lead, name='add_lead'),
     path('<int:pk>/', views.leads_detail, name='leads_detail'),
     path('<int:pk>/delete/', views.leads_delete, name='leads_delete'),
+    path('<int:pk>/edit/', views.leads_edit, name='leads_edit'),
     path('', views.leads_list, name='leads_list'),
 ]
--- crm/lead/views.py
+++ crm/lead/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 from .forms import AddLeadForm
 from .models import Lead
 
@@ -8,9 +8,9 @@ from .models import Lead
 def leads_list(request):
     leads = Lead.objects.filter(created_by=request.user)
 
-    return render(request, 'lead/leads_list.html',{
-        'leads': leads
-    })
+    context = {'leads': leads}
+
+    return render(request, 'lead/leads_list.html',context)
 
 
 @login_required
@@ -18,6 +18,7 @@ def leads_detail(request,pk):
     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
 
     context = {'lead': lead}
+
     return render(request, 'lead/leads_detail.html', context)
 
 @login_required
@@ -25,7 +26,27 @@ def leads_delete(request,pk):
     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
     lead.delete()
 
+    messages.success(request, 'The lead was deleted.')
     return redirect('leads_list')
+
+@login_required
+def leads_edit(request,pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST, instance=lead)
+
+        if form.is_valid():
+            lead.save()
+
+            messages.success(request, 'The changes are saved.')
+            return redirect('leads_list')
+    else:
+        form =  AddLeadForm(instance=lead)
+
+    return render(request, 'lead/leads_edit.html',{
+        'form': form
+    })
 
 @login_required
 def add_lead(request):
@@ -37,7 +58,9 @@ def add_lead(request):
             lead.created_by = request.user
             lead.save()
 
-            return redirect('dashboard')
+            messages.success(request, 'The lead was created.')
+
+            return redirect('leads_list')
     else:
         form = AddLeadForm()
     return render(request, 'lead/add_lead.html',{
